e131782b1aea34eb44e3125401b6d272e20d5ffd
liupan123 <lpmq2011@126.com>
2016-03-22 15:09:28 +0800

    update by liupan

--- fangte/app.js
+++ fangte/app.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+var http = require('http');
+http.createServer(function(req, res){
+	res.writeHead(200,{'Content-Type':'text/html'});
+	res.write('<h1>Node.js</h1>');
+	res.end('<p>PACT</p>')
+}).listen(3000);
+console.log('HTTP ')
